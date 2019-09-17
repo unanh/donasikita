e9dbe97a4218b3c2674faaa50bdbccf5775de729
Caraka572 <fajar_aryawirawan@yahoo.com>
2019-09-17 09:37:00 +0700

    pust

---
+++ tampil/footer.php
@@ -0,0 +1,22 @@
+</div>
+<div class="site-content"></div>
+<div class="ui stackable segment grid">
+
+
+  <div class="ui teal row">
+        <div class="ui sixteen wide column center aligned">
+           <div class="footer">  <a class="item"  href="http://gondoarum.jstore.co"><p align="center"> VC.Arkancom | 2019</p></a></div>
+        </div>
+  </div>
+ 
+  
+
+
+
+
+
+</body>
+</html>
+
+
+ 
---
+++ tampil/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="semantic/style.css">
+	<link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">
+	<script src="semantic/jquery.min.js" type="text/javascript"></script>
+	<script src="semantic/semantic.min.js" type="text/javascript"></script>
+	 <script type="text/javascript">
+    $(document).ready(function(){
+      $('.ui.dropdown').dropdown()
+      ;
+    });
+  </script>
+</head>
+<body class="site">
+<div class="ui hidden divider " ></div>
+<div class="ui grid ">
+<div class="ui equal width grid container segment">
+
+`   <div class="ui row middle aligned">
+			<div class="fourteen wide column">
+			    <div class="ui icon input">
+                <input type="text" placeholder="search">
+                <i class="interted circular search link icon"></i>
+                </div> 
+			</div>
+	</div>
+  
+    <div class="ui blue row width grid container segment">
+      
+		<div class="column center aligned"><h1 class="ui  header">KOPERASI</h1>
+
+        <table>       
+            <tr>        
+            
+                <td><div class=" ui menu">
+                <a href="index.php" class="active item">Beranda</a>
+                </div></td>
+
+                <td> <div class=" ui menu">
+                <a class="active item">Profil</a>
+                </div></td>
+
+                
+           
+            </tr>
+        </table>
+	</div>
+</div>
+<!-- <div class="ui hidden divider " ></div> -->
+<!-- <div class="ui equal width grid container segment">
+</div> -->
+<div class="ui stackable grid full">
+
+
+</div>
+</div>
