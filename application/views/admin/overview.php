1826f4243a0da82c1bf7d48ac18f1643385fe1dd
unanh <nurhusna995@gmail.com>
2019-09-19 13:59:59 +0700

    overview

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -2,109 +2,17 @@
 <html lang="en">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>ADMIN - DONASIKITA</title>
-  <!-- Bootstrap core CSS-->
-  <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-
-  <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
-
-  <!-- Page level plugin CSS-->
-  <link href="<?php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="<?php echo base_url('css/sb-admin.css') ?>" rel="stylesheet">
-
-  
+  <?php $this->load->view("admin/_partials/head.php") ?>
 </head>
 
 <body id="page-top">
-
-  <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-      <img src="<?php echo base_url('assets/img/logo.png') ?>">
-    </button>
-
-    <a class="navbar-brand mr-1" href="index.html">DONASIKITA</a>
-
-    
-    <!-- Navbar -->
-    <ul class="navbar-nav ml-auto ml-md-0">
-     
-     
-      <li class="nav-item dropdown no-arrow">
-        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src="<?php echo base_url('assets/img/pspto.jpg') ?>" class="rounded-circle" alt="Admin" width="50" height="30"> admin <i class='fas fa-angle-down'></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">Settings</a>
-          <a class="dropdown-item" href="#">Activity Log</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-        </div>
-      </li>
-    </ul>
-
-  </nav>
-
+  <?php $this->load->view("admin/_partials/navbar.php") ?>
   <div id="wrapper">
-
-    <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span>
-        </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-          <i class='fas fa-pen'></i>
-          <span>Post</span>
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <a class="dropdown-item" href="login.html">User dan Member</a>
-          <a class="dropdown-item" href="register.html">Donatur dan Donasi</a>
-          <a class="dropdown-item" href="forgot-password.html">Penggalang Dana dan Kampanye</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class='fas fa-user'></i>
-          <span>Kelola Admin</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class='fas fa-sign-out-alt'></i>
-          <span>Keluar</span></a>
-      </li>
-    </ul>
-
+      <?php $this->load->view("admin/_partials/sidebar.php") ?>
     <div id="content-wrapper">
 
       <div class="container-fluid">
-
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Overview</li>
-        </ol>
+          <?php //$this->load->view("admin/_partials/breadbrumb.php") ?>
 
         <!-- Icon Cards-->
         <div class="row">
@@ -680,67 +588,17 @@
       </div>
       <!-- /.container-fluid -->
 
-      <!-- Sticky Footer -->
-      <footer class="sticky-footer">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright © Your Website 2019</span>
-          </div>
-        </div>
-      </footer>
-
+       <!-- Sticky Footer -->
+       <?php $this->load->view("admin/_partials/footer.php") ?>
     </div>
     <!-- /.content-wrapper -->
 
   </div>
   <!-- /#wrapper -->
-
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <script src="<?php echo base_url('assets/fontawesome-free/js/all.min.js') ?>" type="text/javascript"></script>
-  <script src="<?php echo base_url('assets/fontawesome-free/js/all.js') ?>" type="text/javascript"></script>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
-  <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>"></script>
-
-  <!-- Page level plugin JavaScript-->
-  <script src="<?php echo base_url('assets/chart.js/Chart.min.js') ?>"></script>
-  <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-  <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap4.js') ?>"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="<?php echo base_url('js/sb-admin.min.js') ?>"></script>
-
-  <!-- Demo scripts for this page-->
-  <script src="<?php echo base_url('js/demo/datatables-demo.js') ?>"></script>
-  <script src="<?php echo base_url('js/demo/chart-area-demo.js') ?>"></script>
-
+  
+  <?php $this->load->view("admin/_partials/scrolltop.php") ?>
+  <?php $this->load->view("admin/_partials/modal.php") ?>
+  <?php $this->load->view("admin/_partials/js.php") ?>
 </body>
 
 </html>
