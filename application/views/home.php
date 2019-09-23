545e99f0243cb2797eb835ac2dd77e70f380a49c
Caraka572 <fajar_aryawirawan@yahoo.com>
2019-09-23 14:26:55 +0700

    push

---
+++ application/views/home.php
@@ -0,0 +1,94 @@
+    <div class="ui vertical stripe quote segment">
+        <div class="ui equal width stackable internally celled grid">
+            <div class="center aligned row">
+                <div class="column">
+                    <h3>"What a Company"</h3>
+                    <p>That is what they all say about us</p>
+                </div>
+                <div class="column">
+                    <h3>"I shouldn't have gone with their competitor."</h3>
+                    <p>
+                        <img class="ui avatar image" src="./semantic/images/logo.png" />
+                        <b>Nan</b> Chief Fun Officer Acme Toys
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ui hidden divider"></div>
+    <div class="ui grid ">
+<div class="ui equal width grid container segment">
+   
+    <div class="ui grid">
+        <div class="four wide column">
+            <img class="ui small centered image" src="semantic/images/logo.png">
+            <h3>Target Rp.2.000.000,- </h3>
+            <center>
+                <h3>BEROBAT SIFAT SOK BAIK &#10004;</h3>
+            </center>
+            <p align="justify">Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+                adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo.
+                Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu
+                per, quas minimum postulant per id.</p>
+            <div class="ui indicating progress">
+                <div class="bar"></div>
+                <div class="label">Terkumpul Rp.7.00.000,- Sisa 1 Hari</div>
+            </div>
+            <a href="kirimdl.php">
+                <div class="ui orange bottom attached button" tabindex="0">Donasi Sekarang </div>
+            </a>
+        </div>
+        <div class="four wide column">
+            <img class="ui small centered image" src="semantic/images/logo.png">
+            <h3>Target Rp.250.000.000,- </h3>
+            <center>
+                <h3>OPERASI OTAK PICIK &#10004;</h3>
+            </center>
+            <p align="justify">Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+                adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo.
+                Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu
+                per, quas minimum postulant per id.</p>
+            <div class="ui indicating progress">
+                <div class="bar"></div>
+                <div class="label">Terkumpul Rp.89.700.000,- Sisa 17 Hari</div>
+            </div><a href="kirimdl.php">
+                <div class="ui orange bottom attached button" tabindex="0">Donasi Sekarang </div>
+            </a>
+        </div>
+        <div class="four wide column">
+            <img class="ui small centered image" src="semantic/images/logo.png">
+            <h3>Target Rp.150.000.000,- </h3>
+            <center>
+                <h3>KEMO HATI KOTOR &#10004;</h3>
+            </center>
+            <p align="justify">Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+                adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo.
+                Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu
+                per, quas minimum postulant per id.</p>
+            <div class="ui indicating progress">
+                <div class="bar"></div>
+                <div class="label">Terkumpul Rp.117.000.000,- Sisa 20 Hari</div>
+            </div><a href="kirimdl.php">
+                <div class="ui orange bottom attached button" tabindex="0">Donasi Sekarang </div>
+            </a>
+        </div>
+        <div class="four wide column">
+            <img class="ui small centered image" src="semantic/images/logo.png">
+            <h3>Target Rp.70.000.000,- </h3>
+            <center>
+                <h3> BERJUANG MELAWAN MALAS &#10004;</h3>
+            </center>
+            <p align="justify">Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores
+                adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo.
+                Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu
+                per, quas minimum postulant per id.</p>
+            <div class="ui indicating progress">
+                <div class="bar"></div>
+                <div class="label">Terkumpul Rp.66.00.000,- Sisa 2 Hari</div>
+            </div><a href="kirimdl.php">
+                <div class="ui orange bottom attached button" tabindex="0">Donasi Sekarang </div>
+            </a>
+        </div>
+</div>
+
+
---
+++ application/views/tampil/footer.php
@@ -0,0 +1,60 @@
+</div>
+</div>
+<div class="ui hidden divider"></div>
+<div class="ui hidden divider"></div>
+<div class="ui inverted blue vertical footer segment">
+        <div class="ui container">
+          <div
+            class="ui stackable inverted divided equal height stackable grid"
+          >
+            <div class="three wide column">
+              <h4 class="ui inverted header">About</h4>
+              <div class="ui inverted link list">
+                <a class="item" href="#root">Sitemap</a>
+                <a class="item" href="#root">Contact Us</a>
+                <a class="item" href="#root">Religious Ceremonies</a>
+                <a class="item" href="#root">Gazebo Plans</a>
+              </div>
+            </div>
+            <div class="three wide column">
+              <h4 class="ui inverted header">Services</h4>
+              <div class="ui inverted link list">
+                <a class="item" href="#root">Banana Pre-Order</a>
+                <a class="item" href="#root">DNA FAQ</a>
+                <a class="item" href="#root">How To Access</a>
+                <a class="item" href="#root">Favorite X-Men</a>
+              </div>
+            </div>
+            <div class="seven wide column">
+              <h4 class="ui inverted header">Footer Header</h4>
+              <p>
+                Extra space for a call to action inside the footer that could
+                help re-engage users.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+	</div>
+	
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        // fix menu when passed
+        $(".masthead").visibility({
+          once: false,
+          onBottomPassed: function() {
+            $(".fixed.menu").transition("fade in");
+          },
+          onBottomPassedReverse: function() {
+            $(".fixed.menu").transition("fade out");
+          }
+        });
+        // create sidebar and attach to menu open
+        $(".ui.sidebar").sidebar("attach events", ".toc.item");
+      });
+    </script>
+  </body>
+</html>
