be1d50f7e74a7a47de52afc4b80779b726cfbf82
unanh <nurhusna995@gmail.com>
2019-10-08 19:28:33 +0700

    unanh

--- application/controllers/admin/penggalang.php
+++ application/controllers/admin/penggalang.php
@@ -3,57 +3,52 @@
 /**
 * 
 */
-class Kampanye extends CI_Controller
+class Penggalang extends CI_Controller
 {
 	
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model("M_kampanye");
+		$this->load->model("M_penggalang");
 		$this->load->library('form_validation');
 	}
 
-	public function index(){
-		$data["kampanye"] = $this->M_kampanye->getAll();
-		$this->load->view("admin/kampanye/list", $data);
-	}
-
 	public function add(){ 
-		$kampanye = $this->M_kampanye;
+		$penggalang = $this->M_penggalang;
 		$validation = $this->form_validation;
-		$validation->set_rules($kampanye->rules());
+		$validation->set_rules($penggalang->rules());
 
 		if ($validation->run()) {
-			$kampanye->save();
+			$penggalang->save();
 			$this->session->set_flashdata('success','Berhasil disimpan');
 		}
 
 		$this->load->view("admin/kampanye/new_form");
 	}
 
-	public function edit($id_kampanye=null)
+	public function edit($id_penggalang=null)
 	{
-		if (!isset($id_kampanye)) redirect('admin/kampanye');
-		$kampanye = $this->M_kampanye;
+		if (!isset($id_penggalang)) redirect('admin/kampanye');
+		$penggalang = $this->M_penggalang;
 		$validation = $this->form_validation;
-		$validation->set_rules($kampanye->rules());
+		$validation->set_rules($penggalang->rules());
 
 		if($validation->run()) {
-			$kampanye->update();
+			$penggalang->update();
 			$this->session->set_flashdata('success','Berhasil disimpan');
 		}
 
-		$data["kampanye"] = $kampanye->getById($id_kampanye);
-		if (!$data["kampanye"]) show_404();
+		$data["penggalang"] = $penggalang->getById($id_penggalang);
+		if (!$data["penggalang"]) show_404();
 
 		$this->load->view("admin/kampanye/edit_form", $data);
 	}
 
-	public function delete($id_kampanye=null)
+	public function delete($id_penggalang=null)
 	{
-		if (!isset($id_kampanye)) show_404();
+		if (!isset($id_penggalang)) show_404();
 		
-		if ($this->M_kampanye->delete($id_kampanye)) {
+		if ($this->M_penggalang->delete($id_penggalang)) {
 			redirect(site_url('admin/kampanye'));
 		}
 	}
